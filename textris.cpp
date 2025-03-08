ed5e56a350d3ee581fe51b382babcda1e5501b76
Vedant Joshi <vedant.ajay.joshi@gmail.com>
2025-03-08 15:27:45 -0500

    idk how to fix color
    jkfldsa

--- textris.cpp
+++ textris.cpp
@@ -16,6 +16,25 @@ using namespace std;
 
 #define fastio cin.tie(nullptr);ios_base::sync_with_stdio(false);
 
+// colors for text and background
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define BG_BLACK   "\033[40m"
+#define BG_RED     "\033[41m"
+#define BG_GREEN   "\033[42m"
+#define BG_YELLOW  "\033[43m"
+#define BG_BLUE    "\033[44m"
+#define BG_MAGENTA "\033[45m"
+#define BG_CYAN    "\033[46m"
+#define BG_WHITE   "\033[47m"
+
 vector<int> piecesCounter(7, 0); // purple, blue, red, yellow, green, orange, light blue
 vector<string> piecesNotation = {"##", "[]", "@@", "{}", "$$", "()", "!!"};
 vector<string> pieceName = {"#T#", "[J]", "@Z@", "{O}", "$S$", "(L)", "!I!"};
@@ -89,7 +108,7 @@ void display() {
                 case -1: if (j%2 == 1) { cout << ". "; }
                          else { cout << "  "; }
                          break;
-                case 0: cout << "##";
+                case 0: cout << MAGENTA << BG_MAGENTA << "##";
                         break;
                 case 1: cout << "[]";
                         break;
